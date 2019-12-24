67fb49a2a72331d766a9a3f4191037cc6f1c3b19
takatoy <takatoy@nii.ac.jp>
2019-12-24 20:40:42 +0900

    [fix] upgrade to 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="nlplib",
-    version="0.0.0",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=reqs.strip().split("\n"),
 )
